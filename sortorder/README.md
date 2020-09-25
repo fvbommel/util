76dc32e4c6b0a321b13316fc477fb72d06c11d55
Frits van Bommel <fvbommel@gmail.com>
2020-09-25 11:51:00 +0200

    Leave a forwarding address for the sortorder package.

---
+++ sortorder/README.md
@@ -0,0 +1,3 @@
+# sortorder has migrated
+
+Please update your references to point to [`"github.com/fvbommel/sortorder"`](https://github.com/fvbommel/sortorder) instead.
---
+++ sortorder/please-migrate.go
@@ -0,0 +1,3 @@
+// Package sortorder has moved to a new import path.
+// Please update your references to point to "github.com/fvbommel/sortorder" instead.
+package sortorder // import "github.com/fvbommel/sortorder"
